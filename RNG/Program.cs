bed4ebd3f76e0b8a4ab81158002b1daa703f274b
0jorojo <nikita.kulakov@tktk.ee>
2024-02-05 17:22:02 +0200

    add code

---
+++ RNG/Program.cs
@@ -0,0 +1,12 @@
+﻿// 
+Random rnd = new Random();
+int myRando;
+int ranSum = 0;
+
+for (int i = 0; i < 3; i++) // repeat x3
+{
+    myRando = rnd.Next(0, 11);  // 0...10
+    ranSum = ranSum + myRando;
+    Console.WriteLine($"Generated number {myRando}");
+}
+Console.WriteLine($"Sum is {ranSum}");
---
+++ Week4Loops/Program.cs
@@ -0,0 +1,10 @@
+﻿// FOR-loop
+// DRY: Dont Repeat Yourself
+
+int i = 0;
+for(i = 0; i < 5; i++) // first step 0; end condition; i++ = i+1
+{ 
+    Console.WriteLine("We don't need no education."); 
+}
+Console.WriteLine(i);
+Console.WriteLine("Well done.");
