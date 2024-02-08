d3a4b166b3e07290c08ab7494c373d897e7c9790
0jorojo <nikita.kulakov@tktk.ee>
2024-02-08 23:20:06 +0200

    added the correct 4th project

---
+++ whileLoops/Program.cs
@@ -0,0 +1,26 @@
+﻿// generate number 1...3, ask for user to guess, generate new
+
+Random rnd = new Random();
+int cpuRandom;
+
+bool loopActive = true;
+
+while (loopActive)
+{
+    cpuRandom = rnd.Next(1, 4);
+    //Console.WriteLine($"cpu has generated {cpuRandom}.");
+    Console.WriteLine("Make a guess, enter a number 1-3");
+    int userNum = Int32.Parse(Console.ReadLine());
+
+    if (userNum == cpuRandom)
+    {
+        Console.WriteLine("Congratulations!");
+        //loopActive = false;
+        break;
+    }
+    else
+    {
+        Console.WriteLine("That's the wrong number!");
+    }
+}
+Console.WriteLine("You're out of the loop.");
